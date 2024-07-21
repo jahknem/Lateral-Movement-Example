dd63b1125a3dd4863533543608e21aef6db52815
Jan Kühnemund <jan@kuehnemund.io>
2024-07-22 01:35:36 +0200

    So yeah, everyhting should work now.

--- sshuser/script.sh
+++ sshuser/script.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-ssh-keyscan -H jumphost >> ~/.ssh/known_hosts
+# output current location
+cd ~
+pwd
+
+ssh-keyscan -H jumphost >> /home/bob/.ssh/known_hosts
 
 eval $(ssh-agent)
-
+ssh-add /home/bob/.ssh/bob
 while true; do
     echo "SSH connect"
 
-ssh-add /root/.ssh/id_rsa
-ssh -A -tt -o StrictHostKeyChecking=no jumphost << 'EOF'
-ssh -tt -o StrictHostKeyChecking=no target "touch /tmp/hello.txt && sleep 120 && exit"
+ssh -A -tt -o StrictHostKeyChecking=no bob@jumphost << 'EOF'
+ssh -tt -o StrictHostKeyChecking=no root@target "touch /tmp/hello.txt && sleep 120 && exit"
 exit
 EOF
+echo "SSH disconnect"
 
-    sleep 1 # sleep 1 second
+    sleep 10 # sleep 1 second
 done
